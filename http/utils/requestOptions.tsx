60f26da652417d5be0c73340eb43a514ffce3c0c
Jed Bartolo <jedbartolo11@gmail.com>
2023-02-12 19:22:08 +0800

    Initial file set ups

---
+++ http/utils/requestOptions.tsx
@@ -0,0 +1,34 @@
+export function requestOptions(method = 'GET', payload = {}, options = {}) {
+    let auth
+    let body
+    const { token } = options
+
+    if (payload?.token !== undefined) {
+        delete payload.token
+    }
+
+    let header = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    if (token !== undefined) {
+        auth = {
+            Authorization: 'Bearer ' + token,
+        }
+
+        header.headers = { ...header.headers, ...auth }
+    }
+
+    let returns = {
+        method: method,
+        ...header,
+    }
+
+    if (method != 'GET' && Object.keys(payload).length !== 0) {
+        returns.body = JSON.stringify(payload)
+    }
+
+    return returns
+}
